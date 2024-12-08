804d5555a060fb58ff59b063b24023d3b751375a
seckinbuyukavcu <asbuyukavcu04@gmail.com>
2024-12-08 18:34:17 +0300

    first commit

---
+++ src/City.java
@@ -0,0 +1,21 @@
+public class City {
+    private String city;
+
+    public City(String city) {
+        this.city = city;
+    }
+    public City() {
+       
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+  
+
+    
+}
